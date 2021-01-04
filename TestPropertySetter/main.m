c86afc87ac7c3de1989e9561c45a7e87392367fd
zhuzhijia <zhuzhijia@lespark.us>
2021-01-04 16:06:46 +0800

    Initial Commit

---
+++ TestPropertySetter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestPropertySetter
+//
+//  Created by 朱志佳 on 2021/1/4.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
